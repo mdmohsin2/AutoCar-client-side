844e89e9304e5b2274485d56f3ea8498f1a75ef3
mdmohsin2 <mohsinbd862554@gmail.com>
2022-11-25 11:49:11 +0600

    sign up page ok

--- src/Page/SignUp/SignUp.js
+++ src/Page/SignUp/SignUp.js
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [signUpError, setSignUpError] = useState('')
+
+
+    // handle sign up
+    const handleSignUp = (data) => {
+        console.log(data);
+        setSignUpError('')
+        toast.success('User Create Successfully')
+        reset()
+
+    }
+
+
+
     return (
         <div>
-           <h2>this is sign up</h2>
+            <div className='h-[680px] flex justify-center items-center'>
+                <div className='w-96 bg-gray-800 rounded-md p-7'>
+                    <h2 className='text-xl text-center font-bold text-cyan-400'>Sign Up</h2>
+                    <form onSubmit={handleSubmit(handleSignUp)}>
+                        <div className="form-control w-full max-w-xs mt-1">
+                            <label className="label"><span className="label-text text-white">Name</span></label>
+                            <input type="text" {...register("name", {
+                                required: 'Name is required'
+                            })} className="input input-bordered input-info w-full max-w-xs" />
+                            {errors.name && <p className='text-error'>{errors.name.message}</p>}
+                        </div>
+                        <div className="form-control w-full max-w-xs mt-2">
+                            <label className="label"><span className="label-text text-white">Email</span></label>
+                            <input type="email" {...register("email", {
+                                required: true
+                            })} className="input input-bordered input-info w-full max-w-xs" />
+                            {errors.email && <p className='text-error'>{errors.email.message}</p>}
+                        </div>
+                        <div className="col-span-6 sm:col-span-3 mt-2">
+                            <label
+                                htmlFor="FirstName"
+                                className="block text-sm font-medium text-white"
+                            >
+                                Are you seller or buyer?
+                            </label>
+
+                            <select
+                                name="option"
+                                className="select select-bordered w-full max-w-xs my-2"
+                                {...register('option', { required: "Option is required" })}
+                            >
+                                <option disabled selected>Who shot first?</option>
+                                <option value="Buyer">Buyer</option>
+                                <option value="Seller">Seller</option>
+                            </select>
+                            {errors.option && <span className='text-error'>{errors.option?.message}</span>}
+                        </div>
+                        <div className="form-control w-full max-w-xs mb-4">
+                            <label className="label"><span className="label-text text-white">Password</span></label>
+                            <input type="password" {...register("password", {
+                                required: 'password is required',
+                                minLength: { value: 6, message: 'password must be 6 characters longer' }
+                            })} className="input input-bordered input-info w-full max-w-xs" />
+                            {errors.password && <p className='text-error'>{errors.password.message}</p>}
+                        </div>
+                        <input type="submit" className='btn bg-gray-600 w-full' />
+                        {signUpError && <p className='text-error'>{signUpError}</p>}
+                    </form>
+                    <p className='mt-1 text-white'>Already have a account <Link to='/login' className='text-cyan-500 ml-2'>Please Login</Link></p>
+                    <div className="divider my-4 text-cyan-400">OR</div>
+                    <button className='btn btn-primary w-full'>CONTINUE WITH GOOGLE</button>
+                </div>
+            </div>
+            <hr />
         </div>
     );
 };
