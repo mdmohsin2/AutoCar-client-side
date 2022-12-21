a7cfe032c4b249a8981dd7832d9734aba9aa9e53
mdmohsin2 <mohsinbd862554@gmail.com>
2022-12-21 22:48:14 +0600

    image and spiner update

--- src/Page/Home/CategoryPage/CategoryPageItems.js
+++ src/Page/Home/CategoryPage/CategoryPageItems.js
@@ -28,7 +28,7 @@ const CategoryPageItems = ({ product, refetch, setData, }) => {
     }
 
     if (loading) {
-        return <progress className="progress progress-error w-56"></progress>
+        return <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
     }
 
 
